91e12162323f19779b95d16324798a32212fb223
higurashitaishi <tanishi0320@icloud.com>
2025-11-01 20:32:51 +0900

    じゃんけんをラジオボタンで実装したjankenradio.htmlとjankenradio.ejsを追加した

--- app5.js
+++ app5.js
@@ -47,12 +47,57 @@ app.get("/janken", (req, res) => {
   if( num==1 ) cpu = 'グー';
   else if( num==2 ) cpu = 'チョキ';
   else cpu = 'パー';
-  // ここに勝敗の判定を入れる
-  // 以下の数行は人間の勝ちの場合の処理なので，
-  // 判定に沿ってあいこと負けの処理を追加する
-  judgement = '勝ち';
-  win += 1;
-  total += 1;
+  if ((hand === 'パー' && cpu === 'グー')||
+      (hand === 'チョキ' && cpu === 'パー')||
+      (hand === 'グー' && cpu === 'チョキ')
+    ){
+      judgement = '勝ち';
+      win += 1;
+      total += 1;
+  }
+  else if(hand === cpu){
+    judgement = 'あいこ';
+  }
+  else {
+    judgement = '負け';
+    total += 1;
+  }
+  const display = {
+    your: hand,
+    cpu: cpu,
+    judgement: judgement,
+    win: win,
+    total: total
+  }
+  res.render( 'janken', display );
+});
+
+app.get("/jankenradio", (req, res) => {
+  let hand = req.query.hand;
+  let win = Number( req.query.win );
+  let total = Number( req.query.total );
+  console.log( {hand, win, total});
+  const num = Math.floor( Math.random() * 3 + 1 );
+  let cpu = '';
+  let judgement = '';
+  if( num==1 ) cpu = 'グー';
+  else if( num==2 ) cpu = 'チョキ';
+  else cpu = 'パー';
+  if ((hand === 'パー' && cpu === 'グー')||
+      (hand === 'チョキ' && cpu === 'パー')||
+      (hand === 'グー' && cpu === 'チョキ')
+    ){
+      judgement = '勝ち';
+      win += 1;
+      total += 1;
+  }
+  else if(hand === cpu){
+    judgement = 'あいこ';
+  }
+  else {
+    judgement = '負け';
+    total += 1;
+  }
   const display = {
     your: hand,
     cpu: cpu,
