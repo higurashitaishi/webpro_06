5b0b254ea0c0b96ff50c65cd0363f355f4152406
higurashitaishi <tanishi0320@icloud.com>
2025-11-29 17:10:19 +0900

    station2にCRUを実装した

--- aapp5.js
+++ aapp5.js
@@ -5,47 +5,6 @@ app.set('view engine', 'ejs');
 app.use("/public", express.static(__dirname + "/public"));
 app.use(express.urlencoded({ extended: true }));
 
-let ramen = [
-  { id:1, code:"習志野市", name:"町田商店　新習志野駅前店"},
-  { id:2, code:"千葉市", name:"杉田家　千葉駅前店"},
-  { id:3, code:"千葉市", name:"蒙古タンメン中本　千葉店"},
-  { id:4, code:"千葉市", name:"野良裏家"},
-  { id:5, code:"松戸市", name:"中華蕎麦とみ田"},
-  { id:6, code:"柏市", name:"王道家　柏店"},
-];
-
-app.get("/ramen_add", (req, res) => {
-  let id = req.query.id;
-  let code = req.query.code;
-  let name = req.query.name;
-  let newdata = { id: id, code: code, name: name };
-  ramen.push( newdata );
-  res.render('db3', { data: ramen });
-});
-
-let station = [
-  { id:1, code:"JE01", name:"東京駅"},
-  { id:2, code:"JE07", name:"舞浜駅"},
-  { id:3, code:"JE12", name:"新習志野駅"},
-  { id:4, code:"JE13", name:"幕張豊砂駅"},
-  { id:5, code:"JE14", name:"海浜幕張駅"},
-  { id:6, code:"JE05", name:"新浦安駅"},
-];
-
-app.get("/keiyo", (req, res) => {
-  // 本来ならここにDBとのやり取りが入る
-  res.render('db1', { data: station });
-});
-
-app.get("/keiyo_add", (req, res) => {
-  let id = req.query.id;
-  let code = req.query.code;
-  let name = req.query.name;
-  let newdata = { id: id, code: code, name: name };
-  station.push( newdata );
-  res.render('db2', { data: station });
-});
-
 let station2 = [
   { id:1, code:"JE01", name:"東京駅", change:"総武本線，中央線，etc", passengers:403831, distance:0 },
   { id:2, code:"JE02", name:"八丁堀駅", change:"日比谷線", passengers:31071, distance:1.2 },
@@ -56,16 +15,9 @@ let station2 = [
   { id:7, code:"JE18", name:"蘇我駅", change:"内房線，外房線", passengers:31328, distance:43.0 },
 ];
 
+// 一覧
 app.get("/keiyo2", (req, res) => {
   // 本来ならここにDBとのやり取りが入る
-  let id = req.query.id;
-  let code = req.query.code;
-  let name = req.query.name;
-  let change = req.query.change;
-  let passengers = req.query.passengers;
-  let distance = req.query.distance;
-  let newdata = { id: id, code: code, name: name, change: change, passengers: passengers, distance: distance };
-  station2.push( newdata );
   res.render('keiyo2', {data: station2} );
 });
 
@@ -126,6 +78,8 @@ app.post("/keiyo2/update/:number", (req, res) => {
   res.redirect('/keiyo2' );
 });
 
+
+
 app.get("/hello1", (req, res) => {
   const message1 = "Hello world";
   const message2 = "Bon jour";
@@ -169,21 +123,12 @@ app.get("/janken", (req, res) => {
   if( num==1 ) cpu = 'グー';
   else if( num==2 ) cpu = 'チョキ';
   else cpu = 'パー';
-  if ((hand === 'パー' && cpu === 'グー')||
-      (hand === 'チョキ' && cpu === 'パー')||
-      (hand === 'グー' && cpu === 'チョキ')
-    ){
-      judgement = '勝ち';
-      win += 1;
-      total += 1;
-  }
-  else if(hand === cpu){
-    judgement = 'あいこ';
-  }
-  else {
-    judgement = '負け';
-    total += 1;
-  }
+  // ここに勝敗の判定を入れる
+  // 以下の数行は人間の勝ちの場合の処理なので，
+  // 判定に沿ってあいこと負けの処理を追加する
+  judgement = '勝ち';
+  win += 1;
+  total += 1;
   const display = {
     your: hand,
     cpu: cpu,
@@ -192,42 +137,6 @@ app.get("/janken", (req, res) => {
     total: total
   }
   res.render( 'janken', display );
-});
-
-app.get("/jankenradio", (req, res) => {
-  let hand = req.query.hand;
-  let win = Number( req.query.win );
-  let total = Number( req.query.total );
-  console.log( {hand, win, total});
-  const num = Math.floor( Math.random() * 3 + 1 );
-  let cpu = '';
-  let judgement = '';
-  if( num==1 ) cpu = 'グー';
-  else if( num==2 ) cpu = 'チョキ';
-  else cpu = 'パー';
-  if ((hand === 'パー' && cpu === 'グー')||
-      (hand === 'チョキ' && cpu === 'パー')||
-      (hand === 'グー' && cpu === 'チョキ')
-    ){
-      judgement = '勝ち';
-      win += 1;
-      total += 1;
-  }
-  else if(hand === cpu){
-    judgement = 'あいこ';
-  }
-  else {
-    judgement = '負け';
-    total += 1;
-  }
-  const display = {
-    your: hand,
-    cpu: cpu,
-    judgement: judgement,
-    win: win,
-    total: total
-  }
-  res.render( 'jankenradio', display );
 });
 
 app.listen(8080, () => console.log("Example app listening on port 8080!"));
